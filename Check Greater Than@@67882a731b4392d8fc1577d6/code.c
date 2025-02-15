2e6c5a9e672393f73133ea5be529900fb7c4088b
Khushi3467 <khushpreet1200.becse24@chitkara.edu.in>
2025-02-15 13:30:36 +0530

    Solution to Check Greater Than

--- Check Greater Than@@67882a731b4392d8fc1577d6/code.c
+++ Check Greater Than@@67882a731b4392d8fc1577d6/code.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+
+
+int main() 
+{
+    int num1, num2;
+
+   
+    printf("Enter two space-separated integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    
+    if (num1 > num2) {
+        printf("true\n");
+    } else {
+        printf("false\n");
+    }
+
+    return 0;
+}
 
